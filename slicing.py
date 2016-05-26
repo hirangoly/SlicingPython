d5a5f6eab8ace610be0235d978ca62f699829780
Rangoly Jain <hirangoly@gmail.com>
2016-05-26 16:13:05 -0700

    Add files via upload

---
+++ slicing.py
@@ -0,0 +1,27 @@
+# slicing
+
+lst_10 = range(10)
+
+print lst_10
+
+# extract alternate numbers - every 2nd item
+print lst_10[::2]
+
+# extract every 3rd number
+print lst_10[::3]
+
+#reverse order
+print lst_10[::-1]
+
+print lst_10[5:]
+
+abc_str = 'abc'
+# extract alternate numbers - every 2nd item
+print abc_str[::2] 
+
+#exact # of itmes required
+lst_10[::2] = [1,1,1,1,1]
+
+print lst_10
+
+
